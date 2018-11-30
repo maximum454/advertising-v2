b14bfcf7949c8b504feeed6141b6b57507c65d35
maxim <maximum454@gmail.com>
2018-11-30 15:53:06 +0600

    Проект

--- src/js/ma.js
+++ src/js/ma.js
@@ -1,4 +1,9 @@
 //= partials/jquery.min.js
 $(function () {
-
+    $(".js-anhor").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 300);
+    });
 });
